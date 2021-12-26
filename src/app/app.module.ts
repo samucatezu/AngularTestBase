6f01d6a84481d3085ab63e42a75ef36135571f4d
Samuel Almeida <barbosasamuel24@gmail.com>
2021-12-26 15:28:53 -0300

    Base layout created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
 
 import { MatIconModule } from '@angular/material/icon';
-
-import { TopBarComponent } from './top-bar/top-bar.component';
 
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -15,20 +15,33 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { CepDetailsComponent } from './cep-details/cep-details.component';
+import { CepHomeComponent } from './cep-home/cep-home.component';
+import { layoutComponent } from './Layout/layout.component';
 
 @NgModule({
-  declarations: [AppComponent, TopBarComponent],
+  declarations: [
+    AppComponent,
+    layoutComponent,
+    CepHomeComponent,
+    CepDetailsComponent,
+  ],
   imports: [
     BrowserModule,
     MatToolbarModule,
     MatButtonModule,
     MatCardModule,
     BrowserAnimationsModule,
-
+    MatSidenavModule,
+    MatDividerModule,
     MatFormFieldModule,
     MatIconModule,
     ReactiveFormsModule,
     HttpClientModule,
+    // RouterModule.forRoot([
+    //   { path: '', component: CepHomeComponent },
+    //   { path: 'cep/:cep', component: CepDetailsComponent },
+    // ]),
   ],
   providers: [],
   bootstrap: [AppComponent],
