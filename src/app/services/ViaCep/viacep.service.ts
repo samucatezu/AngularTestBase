0e3d7a8b0e8ffb6af7ce1d4eaaded73303295a00
Samuel Almeida <barbosasamuel24@gmail.com>
2021-12-26 19:48:53 -0300

    First feature (AngularViacep) added to the project and code refactor

---
+++ src/app/services/ViaCep/viacep.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CEP } from '../modules/cep';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ViacepService {
+  constructor(private http: HttpClient) {}
+
+  getCep(cep: String) {
+    return this.http.get<CEP>('https://viacep.com.br/ws/' + cep + '/json/');
+  }
+}
