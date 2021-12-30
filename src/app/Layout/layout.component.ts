f5a10a6b6271100aa6fc66d037911921124a1a84
Samuel Almeida <barbosasamuel24@gmail.com>
2021-12-30 19:50:58 -0300

    layout finished

--- src/app/Layout/layout.component.ts
+++ src/app/Layout/layout.component.ts
@@ -1,31 +1,16 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import { BreakpointObserver } from '@angular/cdk/layout';
-import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
-import { MatDialog, } from '@angular/material/dialog';
-
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
 })
-export class layoutComponent implements OnInit {
+export class layoutComponent {
   @ViewChild(MatSidenav)
   sidenav!: MatSidenav;
 
-  
-
-  constructor(
-    private observer: BreakpointObserver,
-    public dialog: MatDialog,
-    private formBuilder: FormBuilder
-    ) {}
-  ngOnInit() {
-    
-  }
-
-  
-  
+  constructor(private observer: BreakpointObserver) {}
 
   ngAfterViewInit() {
     this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
